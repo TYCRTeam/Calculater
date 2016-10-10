97a81ad2f431ca12e6c1c6f94c8bdf17787a7b69
wenwu1993 <290611579@qq.com>
2016-10-10 20:37:58 +0800

    the frame1

---
+++ Calculate/register.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Calculate
+{
+    partial class register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_OK = new System.Windows.Forms.Button();
+            this.button_cancle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_userId = new System.Windows.Forms.TextBox();
+            this.textBox_userPSW = new System.Windows.Forms.TextBox();
+            this.textBox_userPSWRe = new System.Windows.Forms.TextBox();
+            this.textBox_userName = new System.Windows.Forms.TextBox();
+            this.textBox_userParent = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_userParentEmail = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox_userSchool = new System.Windows.Forms.TextBox();
+            this.textBox_userClass = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_OK
+            // 
+            this.button_OK.Location = new System.Drawing.Point(180, 559);
+            this.button_OK.Name = "button_OK";
+            this.button_OK.Size = new System.Drawing.Size(75, 23);
+            this.button_OK.TabIndex = 0;
+            this.button_OK.Text = "确定";
+            this.button_OK.UseVisualStyleBackColor = true;
+            // 
+            // button_cancle
+            // 
+            this.button_cancle.Location = new System.Drawing.Point(372, 559);
+            this.button_cancle.Name = "button_cancle";
+            this.button_cancle.Size = new System.Drawing.Size(75, 23);
+            this.button_cancle.TabIndex = 1;
+            this.button_cancle.Text = "取消";
+            this.button_cancle.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "学生姓名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "账号：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "密码：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "密码确认：";
+            // 
+            // textBox_userId
+            // 
+            this.textBox_userId.Location = new System.Drawing.Point(105, 30);
+            this.textBox_userId.Name = "textBox_userId";
+            this.textBox_userId.Size = new System.Drawing.Size(167, 21);
+            this.textBox_userId.TabIndex = 6;
+            // 
+            // textBox_userPSW
+            // 
+            this.textBox_userPSW.Location = new System.Drawing.Point(105, 63);
+            this.textBox_userPSW.Name = "textBox_userPSW";
+            this.textBox_userPSW.Size = new System.Drawing.Size(167, 21);
+            this.textBox_userPSW.TabIndex = 7;
+            // 
+            // textBox_userPSWRe
+            // 
+            this.textBox_userPSWRe.Location = new System.Drawing.Point(105, 96);
+            this.textBox_userPSWRe.Name = "textBox_userPSWRe";
+            this.textBox_userPSWRe.Size = new System.Drawing.Size(167, 21);
+            this.textBox_userPSWRe.TabIndex = 8;
+            // 
+            // textBox_userName
+            // 
+            this.textBox_userName.Location = new System.Drawing.Point(105, 50);
+            this.textBox_userName.Name = "textBox_userName";
+            this.textBox_userName.Size = new System.Drawing.Size(167, 21);
+            this.textBox_userName.TabIndex = 9;
+            // 
+            // textBox_userParent
+            // 
+            this.textBox_userParent.Location = new System.Drawing.Point(105, 176);
+            this.textBox_userParent.Name = "textBox_userParent";
+            this.textBox_userParent.Size = new System.Drawing.Size(167, 21);
+            this.textBox_userParent.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "家长姓名：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox_userPSW);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox_userPSWRe);
+            this.groupBox1.Controls.Add(this.textBox_userId);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(330, 153);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "账号信息";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "学校：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 131);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "年级：";
+            // 
+            // textBox_userParentEmail
+            // 
+            this.textBox_userParentEmail.Location = new System.Drawing.Point(105, 214);
+            this.textBox_userParentEmail.Name = "textBox_userParentEmail";
+            this.textBox_userParentEmail.Size = new System.Drawing.Size(167, 21);
+            this.textBox_userParentEmail.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 217);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 12);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "家长邮箱：";
+            // 
+            // textBox_userSchool
+            // 
+            this.textBox_userSchool.Location = new System.Drawing.Point(105, 88);
+            this.textBox_userSchool.Name = "textBox_userSchool";
+            this.textBox_userSchool.Size = new System.Drawing.Size(167, 21);
+            this.textBox_userSchool.TabIndex = 17;
+            // 
+            // textBox_userClass
+            // 
+            this.textBox_userClass.Location = new System.Drawing.Point(105, 128);
+            this.textBox_userClass.Name = "textBox_userClass";
+            this.textBox_userClass.Size = new System.Drawing.Size(167, 21);
+            this.textBox_userClass.TabIndex = 18;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_userParent);
+            this.groupBox2.Controls.Add(this.textBox_userClass);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox_userSchool);
+            this.groupBox2.Controls.Add(this.textBox_userName);
+            this.groupBox2.Controls.Add(this.textBox_userParentEmail);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(12, 195);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(330, 329);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "学生信息";
+            // 
+            // register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 597);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button_cancle);
+            this.Controls.Add(this.button_OK);
+            this.Name = "register";
+            this.Text = "欢迎";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.Button button_cancle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_userId;
+        private System.Windows.Forms.TextBox textBox_userPSW;
+        private System.Windows.Forms.TextBox textBox_userPSWRe;
+        private System.Windows.Forms.TextBox textBox_userName;
+        private System.Windows.Forms.TextBox textBox_userParent;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_userParentEmail;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox_userSchool;
+        private System.Windows.Forms.TextBox textBox_userClass;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
